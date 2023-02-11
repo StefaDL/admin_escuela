fee2dd8e166f7952435c6527b933688cfedd1acf
AndresAlvaradow <andresalvarado34@gmail.com>
2023-02-10 21:49:01 -0500

    crear modelo materia

--- apps/school/models.py
+++ apps/school/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Auto(models.Model):
-    quimica = models.AutoField(primary_key=True)
-    fisica = models.TextField()
-    ingles = models.TextField()
-    matematicas = models.IntegerField()
-    lenjuaje = models.TextField()
+class Materia(models.Model):
+    id_materia = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=65)
+    num_alumn = models.IntegerField()
+    anio_lec = models.IntegerField()
+    descripcion = models.TextField()
+
--- apps/school/urls.py
+++ apps/school/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'materia'
+app_name = 'school'
 
 urlpatterns = [
-    path('',)
+    path('',views.materia, name='index')
 ]
