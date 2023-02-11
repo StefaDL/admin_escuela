5cc50cada300ef760db97089c4e58b5fc30a4487
StefaDL <stefanylucio10@gmail.com>
2023-02-10 21:28:03 -0500

    cambiostefa

--- apps/school/models.py
+++ apps/school/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+    quimica = models.AutoField(primary_key=True)
+    fisica = models.TextField()
+    ingles = models.TextField()
+    matematicas = models.IntegerField()
+    lenjuaje = models.TextField()
--- apps/school/views.py
+++ apps/school/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def materia(request):
+    return HttpResponse(f"<h1>hola<h1>")
