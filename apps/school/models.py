0465b60f9bbfab1577c742b1102cce89d44d1147
AndresAlvaradow <andresalvarado34@gmail.com>
2023-02-27 21:34:10 -0500

    creacion modelos fase 1

--- apps/school/models.py
+++ apps/school/models.py
@@ -7,3 +7,11 @@ class Materia(models.Model):
     anio_lec = models.IntegerField()
     descripcion = models.TextField()
 
+    def __str__(self):
+        return self.nombre
+
+class Docente(models.Model):
+    id_docente = models.CharField(max_length=12, primary_key=True)
+    nombre = models.CharField(max_length=65)
+    apellido = models.CharField(max_length=65)
+    materia = models.ForeignKey(Materia, null=False, on_delete=models.CASCADE)
